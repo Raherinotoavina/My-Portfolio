91cbdf54d87dc739de1cad01de01836924e4e286
raherinotoavina <raherinotoavinasa@gmail.com>
2023-04-13 18:52:29 +0300

    creating the repository

---
+++ src/PortFolio.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Header, Footer } from "./components";
+
+const PortFolio = () => {
+    return (
+        <div className="app">
+            <Header />
+            <main>
+                Hello world
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default PortFolio;
---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/style.scss";
+import App from "./PortFolio";
+
+ReactDOM
+    .createRoot(document.getElementById("root"))
+    .render(
+        <React.StrictMode>
+            <App />  
+        </React.StrictMode>
+    )
